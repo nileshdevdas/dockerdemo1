c04b6732f75338c925ec2b4f146871eb91ef0508
nilesh.devdas@vinsys.com <nilesh.devdas@vinsys.com>
2020-07-24 12:26:35 +0530

    Build

---
+++ index.js
@@ -0,0 +1,13 @@
+var  express = require("express");
+var app = express();
+var port = 3000;
+
+app.get("/hello", function(req,res){
+
+    res.send("Thank You Microservice");
+    res.end("Ending");
+});
+
+app.listen(port, function(){
+    console.log("Listening")
+});
